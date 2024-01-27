fc729c1c8a5d123d2521f0a80777efcefb7ebb68
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-01-27 10:47:28 -0800

    2. Add Two Numbers

--- src/Main.java
+++ src/Main.java
@@ -42,9 +42,6 @@ static class ListNode {
          ListNode(int val) { this.val = val; }
          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
      }
-
-    ListNode result = new ListNode(0);
-    ListNode ptr = result;
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode result = new ListNode(0);
         ListNode ptr = result;

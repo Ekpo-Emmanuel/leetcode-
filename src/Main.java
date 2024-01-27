3746b8d11ac8bce90c3821e3e907f14b3601cf2f
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-01-27 11:34:20 -0800

    415. Add Strings

--- src/Main.java
+++ src/Main.java
@@ -34,8 +34,8 @@ public class Main {
     }
 
 
-//     2. Add Two Numbers
-static class ListNode {
+    //2. Add Two Numbers
+    static class ListNode {
          int val;
          ListNode next;
          ListNode() {}
@@ -74,4 +74,39 @@ static class ListNode {
 
         return result.next;
     }
+
+    //415. Add Strings
+    public String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+
+        int p1 = num1.length() - 1;
+        int p2 = num2.length() - 1;
+        int carry = 0;
+
+        while(p1 >= 0 || p2 >= 0) {
+            int sum = carry;
+
+            if(p1 >= 0) {
+                sum += num1.charAt(p1) - '0';
+                p1--;
+            }
+
+            if(p2 >= 0) {
+                sum += num2.charAt(p2) - '0';
+                p2--;
+            }
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            result.append(sum);
+        }
+
+        if (carry != 0) {
+            result.append(carry);
+        }
+
+        return result.reverse().toString();
+    }
 }
+

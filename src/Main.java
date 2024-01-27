954f9768ddb138c43852537c16061d732b81992d
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-01-27 10:46:23 -0800

    2. Add Two Numbers

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        
-    }
 
     // 1143. Longest Common Subsequence
     public int longestCommonSubsequence(String text1, String text2) {
@@ -34,5 +31,50 @@ public class Main {
         }
 
         return dp[len1];
+    }
+
+
+//     2. Add Two Numbers
+static class ListNode {
+         int val;
+         ListNode next;
+         ListNode() {}
+         ListNode(int val) { this.val = val; }
+         ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     }
+
+    ListNode result = new ListNode(0);
+    ListNode ptr = result;
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(0);
+        ListNode ptr = result;
+        int carry = 0;
+
+        while(l1!=null || l2!=null) {
+            int sum = carry;
+
+            if(l1!=null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2!=null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+            sum = sum % 10;
+
+            ptr.next = new ListNode(sum);
+            ptr = ptr.next;
+        }
+
+        if(carry == 1) {
+            ptr.next = new ListNode(1);
+            ptr = ptr.next;
+        }
+
+        return result.next;
     }
 }

d7dcd9a5980f0dc1847dc9520c82a8584d3ce313
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-02-06 14:30:49 -0800

    283. Move Zeroes

--- src/Main.java
+++ src/Main.java
@@ -255,5 +255,19 @@ public class Main {
         }
         return nums;
     }
+
+    //    283. Move Zeroes
+    public void moveZeroes(int[] nums) {
+        int j = 0;
+        for (int i =0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+
+        while(j < nums.length) {
+            nums[j++] = 0;
+        }
+    }
 }
 

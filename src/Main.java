e67952a94dfbd695f3b9f4d11e34e25764727cd2
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-02-06 23:32:01 -0800

    189. Rotate Array

--- src/Main.java
+++ src/Main.java
@@ -307,5 +307,24 @@ public class Main {
 
     return j;
 }
+
+    //189. Rotate Array
+    public void rotate(int[] nums, int k) {
+        if (nums.length <=1 ) return;
+        k %= nums.length;
+        reverse(nums, 0, nums.length -1);
+        reverse(nums, 0, k -1);
+        reverse(nums, k, nums.length-1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
 }
 

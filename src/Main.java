c8af7c7e8e6139ecad3ee9a053369c30c9bcfb75
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-02-09 23:36:11 -0800

    9. Palindrome Number

--- src/Main.java
+++ src/Main.java
@@ -339,5 +339,21 @@ public class Main {
 
         return maxSoFar;
     }
+
+    // 9. Palindrome Number
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int originalNum = x;
+        int reversedNum = 0;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reversedNum = (reversedNum * 10) + digit;
+            x /= 10;
+        }
+
+        return originalNum == reversedNum;
+    }
 }
 

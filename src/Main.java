f1ebd2be3f3bf2e5be69a388d74c77ece884f8d7
Ekpo-Emmanuel <ekpoemmanuelsg@gmail.com>
2024-02-14 07:51:35 -0800

    3. Longest Substring Without Repeating Characters

--- src/Main.java
+++ src/Main.java
@@ -355,5 +355,24 @@ public class Main {
 
         return originalNum == reversedNum;
     }
+
+    //3. Longest Substring Without Repeating Characters
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int max = 0;
+        int start = 0;
+
+        for (int end = 0; end < s.length(); end++) {
+            char current = s.charAt(end);
+
+            if(map.containsKey(current)) {
+                start = Math.max(start, map.get(current) + 1);
+            }
+            max = Math.max(max, end - start + 1);
+            map.put(current, end);
+        }
+
+        return max;
+    }
 }
 
